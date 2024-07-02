89535640bab8f6d17f5ba6b05be6356f4eca55a4
Juan Medeiros <juan_ormelli@hotmail.com>
2024-07-01 22:29:39 -0300

    Basic RabbitMQ Implementation

---
+++ Consumer/Program.cs
@@ -0,0 +1,75 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+
+namespace Consumer
+{
+  class Teste
+  {
+    static void Main(string[] args)
+    {
+
+      Console.ReadLine();
+      //Configurar Conexao 
+      var factory = new ConnectionFactory()
+      {
+        HostName = "localhost"
+      };
+
+
+      // Abrir conexao
+      using (var connection = factory.CreateConnection())
+
+      // Criar um Canal onde vamos definir uma mensagem e publicar a mensagem 
+      using (var channel = connection.CreateModel())
+      {
+        // Declaramos a fila de onde vamos consumir nossa mensagens 
+
+        /*
+         Queue:  Nome da fila 
+         Durable: Se for igual a true a fila segue ativa apos o reinicio do sever 
+         exclusive: Se for true ela so pode ser acessada via a conexao atual e as fila sao excluidas quando a conexao fecha 
+         Auto Delete: se igual a true a mensagem sera deletada ´pós os consumidores usarem a fila
+         */
+        channel.QueueDeclare(queue: "Teste Juan",
+                             durable: false,
+                             exclusive: false,
+                             autoDelete: false,
+                             arguments: null);
+
+        // Solicita a entrega das nossas mensagne s
+
+        var consumer = new EventingBasicConsumer(channel);
+          Console.WriteLine("Aqui");
+
+
+        // Após isso vamos receber nossa smensagens do nosso briker
+        consumer.Received += async (model, ea) =>
+        {
+          Console.WriteLine("Aqui");
+          //Transformamos a mensgame num array 
+          var body = ea.Body.ToArray();
+
+          //Convertemos nossa mensagem para a linguagem ou padrao utf-8
+          var message = Encoding.UTF8.GetString(body);
+
+          Console.WriteLine($"Mensagem recebida: {message}");
+          Console.ReadLine();
+        };
+
+        // Após isso vamos colocar nossa mensagem como consumida no Rabbit MQ
+
+        channel.BasicConsume(queue: "Teste Juan",
+                              autoAck: true,
+                              consumer: consumer);
+
+
+        Console.ReadLine();
+
+
+
+      }
+    }
+  }
+}
---
+++ Publisher/Program.cs
@@ -0,0 +1,57 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace Publisher
+{
+  class Program
+  {
+
+    static void Main(string[] args)
+    {
+
+
+      //Configurar Conexao 
+      var factory = new ConnectionFactory()
+      {
+        HostName = "localhost"
+      };
+
+
+      // Abrir conexao
+      using (var connection = factory.CreateConnection())
+
+      // Criar um Canal onde vamos definir uma mensagem e publicar a mensagem 
+      using (var channel = connection.CreateModel())
+      {
+        // Criação da fila definindo alguns parametros como:
+
+        /*
+         Queue:  Nome da fila 
+         Durable: Se for igual a true a fila segue ativa apos o reinicio do sever 
+         exclusive: Se for true ela so pode ser acessada via a conexao atual e as fila sao excluidas quando a conexao fecha 
+         Auto Delete: se igual a true a mensagem sera deletada ´pós os consumidores usarem a fila
+         */
+        channel.QueueDeclare(queue: "Teste Juan",
+                             durable: false,
+                             exclusive: false,
+                             autoDelete: false,
+                             arguments: null);
+
+        // Criamos a mensagem 
+        string Message = "Hello World";
+
+
+        //Codificamos para bytes 
+        var body = Encoding.UTF8.GetBytes(Message);
+
+        //Pucllicamos a mensagem com o nome da dila e a mensagem como corpo da mensagem 
+        channel.BasicPublish(exchange: "",
+                             routingKey: "Teste Juan",
+                             basicProperties: null,
+                             body: body);
+
+        Console.WriteLine(Message + " Enviada!");
+      }
+    }
+  }
+}
